e37aebc26003c672279c93dbaaaf0b96fd897b60
serna37 <serna37@serna37noMacBook-Pro.local>
2024-07-03 13:26:51 +0900

    tmp

---
+++ work3/1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+// =====================================
+// XXX 提出前に要削除
+#include <bits/debug.hpp>
+// =====================================
+// ⭐️ fastio_macro
+// ⭐️ INF__MOD__GRID__PI__EPS__MAX
+// =====================================
+int main() {
+    // fastio_tie
+    // cout_fixed_setprecision
+    int N;
+    cin >> N;
+    string S;
+    cin >> S;
+    cout << S.find("ABC") + 1 << endl;
+}
